d7e4c0974289cbd40dbdd92dd7c753671320b18a
tianle cheng <chengtianle1997@163.com>
2018-12-21 22:03:32 +0800

    Add project files.

---
+++ SocketFinal/ClientMain.cpp
@@ -0,0 +1,125 @@
+
+#include<iostream>
+
+#include<string>
+
+#include"ClientNet.h"
+
+
+
+using namespace std;
+
+typedef struct {
+
+	int CameraNum;
+	int Framecnt;
+	float Res[4096];
+
+}SocketPackage;
+
+SocketPackage Sockpack;
+
+
+int main()
+
+{
+
+	ClientNet client;
+
+	int rlt = 0;
+
+	int size = 2048;
+
+	string msg;
+
+	//memset(msg, 0, sizeof(msg));
+
+	//SocketPackage Sockpack;
+
+	Sockpack.CameraNum = 1;
+	Sockpack.Framecnt = 1;
+	//Sockpack.Res = new float(size);
+
+	for (int i = 0; i < 2048; i++)
+		Sockpack.Res[i] = 123.0;
+
+	char* buf = (char*)malloc(sizeof(SocketPackage));
+
+	memcpy(buf, &Sockpack, sizeof(SocketPackage));
+
+	//连接到127.0.0.1（即本地）,端口号为8888的服务端
+
+	printf("connecting.....\n");
+
+	rlt = client.ClientConnect(8888, "127.0.0.1");
+
+
+
+
+	if (rlt == 0)
+
+		// socket连接成功
+
+	{
+
+		//发送消息
+
+		//printf("	connect successfully. input  q to quit\n");
+
+		//printf("-------------------------\n");
+
+		//while (1)
+
+		//{
+
+			//printf("msg input: ");
+
+			//getline(cin, msg);
+
+			//if (msg == "q")
+
+				//break;
+
+			//else
+
+			//{
+
+		printf("sending msg.....\n");
+
+
+
+		rlt = client.ClientSend(buf, sizeof(Sockpack));
+
+		//}
+
+
+
+	//}
+
+
+
+
+
+	// 关闭socket
+
+		printf("closing socket.....\n");
+
+		client.ClientClose();
+
+
+
+
+
+	}
+
+
+
+	system("pause");
+
+
+
+	return 0;
+
+
+
+}
